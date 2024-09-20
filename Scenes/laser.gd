c5200788849d38b0ca7386504f8b51db7765dcd9
LavanGrimwulff <31390396+LavanGrimwulff@users.noreply.github.com>
2024-09-20 07:50:03 -0500

    Sounds added
    Added sounds to player shooting, player death, and invader death

--- Scenes/laser.gd
+++ Scenes/laser.gd
@@ -3,6 +3,11 @@ extends Area2D
 class_name Laser
 
 @export var speed = 300
+const SI_PLAYER_SHOOT = preload("res://Assets/Audio/SI_Player_Shoot.wav")
+
+func _ready():
+	$AudioStreamPlayer2D.stream = SI_PLAYER_SHOOT
+	$AudioStreamPlayer2D.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
