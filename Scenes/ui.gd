5b63ebeda23411c26794601f27dbffdf1dcd4469
LavanGrimwulff <31390396+LavanGrimwulff@users.noreply.github.com>
2024-10-13 15:15:42 -0500

    Visibility setting issue
    Fixed a visibility setting I had flagged for testing.

--- Scenes/ui.gd
+++ Scenes/ui.gd
@@ -5,12 +5,15 @@ var life_texture = preload("res://Assets/Player/Player.png")
 @onready var lifes_ui_container = $MarginContainer/HBoxContainer
 
 @onready var center_container = %CenterContainer
+@onready var center_container_2: CenterContainer = %CenterContainer2
+
 @onready var points_label = $MarginContainer/Points
 @onready var points_counter = $"../PointsCounter" as PointsCounter
 @onready var label = $MarginContainer/CenterContainer/GameOverBox/Label
 @onready var restart_button: Button = $MarginContainer/CenterContainer/GameOverBox/Restart
-@onready var submit_score: Button = $MarginContainer/CenterContainer/GameOverBox/SubmitScore
+@onready var submit_score: Button = $"MarginContainer/CenterContainer2/Score Submit Box/SubmitScore"
 @onready var text_edit: TextEdit = $"MarginContainer/CenterContainer2/Score Submit Box/TextEdit"
+@onready var score_submission_screen: Button = $MarginContainer/CenterContainer/GameOverBox/ScoreSubmissionScreen
 
 @export var invader_spawner: InvaderSpawner
 @export var life_manager: LifeManager
@@ -24,6 +27,7 @@ func _ready():
 	invader_spawner.game_won.connect(on_game_won)
 	restart_button.pressed.connect(on_restart_button_press)
 	submit_score.pressed.connect(_on_submit_score_pressed)
+	score_submission_screen.pressed.connect(_on_score_submission_screen_pressed)
 	life_manager.on_life_lost.connect(on_life_lost)
 	
 	var lifes_count = life_manager.lifes
@@ -51,11 +55,17 @@ func on_game_won():
 func on_restart_button_press():
 	get_tree().reload_current_scene()
 	
-func _on_submit_score_pressed():
+func _on_submit_score_pressed(): #submits score and returns to main menu
 	if(text_edit.text != ""):
 		playername = text_edit.text
 		SilentWolf.Scores.save_score(playername,Globals.score)
 		get_tree().change_scene_to_file("res://start_screen.tscn")
+		
+func _on_score_submission_screen_pressed(): #Makes the restart/submit screen invisible and displays the submit score screen
+	center_container.visible = false
+	center_container_2.visible = true
+	
+
 
 func on_life_lost(lifes_left:int):
 	print_debug(lifes_left)
